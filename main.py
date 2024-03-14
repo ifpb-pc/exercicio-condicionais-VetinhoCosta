0b130cc23d4e671f263425ffa32dcf7e917471ce
VetinhoCosta <112962380+VetinhoCosta@users.noreply.github.com>
2024-03-14 20:13:15 +0000

    modificacao da questao9

--- main.py
+++ main.py
@@ -136,20 +136,20 @@ def q9():
       indicando se a pessoa está: Abaixo do peso, Peso normal, Sobrepeso, 
       Obeso ou Muito obesa.
     """
-    peso = float(input('digite seu peso: '))
     altura = float(input('Digite sua altura: '))
+    peso = float(input('digite seu peso: '))
     imc = peso / altura ** 2
     
-    if imc <= 18.5:
-        print('Abaixo do peso')
-    elif imc >=18.6 and imc <= 24.9:
+    if imc < 18.5:
+        print('Peso normal')
+    elif imc >=18.5 and imc <= 24.9:
         print('Peso normal') 
     elif imc >= 25.0 and imc <= 29.9:
         print('Sobrepeso')
     elif imc >= 30.0 and imc <= 34.9:
         print('Obeso')
-    else:
-        print('Muito obeso')    
+    elif imc >= 35.0 and imc <= 39.9:
+        print('Muito obeso')
 
 
 def q10():
